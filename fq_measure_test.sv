10af4e23c33d2bf73786399a087610c83900fe73
nil1g18 <99904618+nil1g18@users.noreply.github.com>
2023-04-09 16:30:56 +0800

    Update fq_measure_test.sv
    Fixed bad testbench :V

--- fq_measure_test.sv
+++ fq_measure_test.sv
@@ -1,4 +1,4 @@
-`define REF_FREQ 1000000
+`define REF_FREQ 1000000   //1MHz
 
 module fq_measure_test;
 
@@ -10,7 +10,7 @@ logic ref_freq;
 logic nReset;
 
 int counter;
-int freq[3];
+int freq[6];
 
 int i = 0;
 
@@ -27,29 +27,31 @@ ref_freq = 0;
 input_freq = 0;
 #10 nReset = 1;
 
-freq[0] = 7482;
-freq[1] = 68921;
-freq[2] = 223;
+freq[0] = 1; //500000
+freq[1] = 13; //38,461
+freq[2] = 223; //2244
+freq[3] = 756; //661
+freq[4] = 3; //166,666
+freq[5] = 9200; //54
 
 #50 
-while(1) #(10000000/(2*freq[i])) input_freq = !input_freq;
+while(1) #(freq[i]) input_freq = !input_freq;
 
 end
 
 initial
 begin
 
-#20000000 i = 1;
-#20000000 i = 2;
-#20000000 $stop;
+for(int j = 0 ; j < 6; j++)
+	#3000000 i = j;
+
+#3000000 $stop;
 
 
 end
 
-
 always
-	#(10000000/(2*`REF_FREQ)) ref_freq = !ref_freq;
-	
+	#0.5 ref_freq = !ref_freq;
 	
 always @(posedge ref_freq)
 	counter <= counter + 1;
